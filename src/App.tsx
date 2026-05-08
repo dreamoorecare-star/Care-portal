1862f7dbf82aee5a9322c292b3ea3dab0b2c1f31
dreamoorecare-star <dreamoorecare@gmail.com>
2026-05-08 20:12:15 +1000

    fix build

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { createClient } from "@supabase/supabase-js";
+//test
 
 const supabase = createClient(
   "https://uktftzmuzgoojrpwgcdi.supabase.co",
